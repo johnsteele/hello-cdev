9eb6fdbadd99678f57f59ee97094ca3a6800eed7
John Steele <programjsteele@gmail.com>
2010-09-21 23:44:04 -0700

    Created the char driver module file.

---
+++ hello_cdev.c
@@ -0,0 +1,32 @@
+/*
+ * cdev.c -- A simple char device driver.
+ *
+ * Copyright (C) 2010 John Stele
+ *
+ * $Id: cdev.c, v 1.0.0 2010/09/21 John Exp $
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>       /* printk() */
+#include <linux/moduleparam.h>  /* command-line args */
+#include <asm/uaccess.h>        /* copy_*_user */
+
+#include <linux/types.h>     /* dev_t types */
+#include <linux/cdev.h>      /* cdev */
+#include <linux/fs.h>        /* file_operations */
+
+static int __init driver_init(void)
+{
+	int result;
+	result = 0;
+	return result;
+}
+
+static void __exit driver_cleanup(void) 
+{
+}
+
+module_init(driver_init);
+module_exit(driver_cleanup);
+
